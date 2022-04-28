bd5cfd74671239face217882fe9f7754ba712a7d
ahbab <sakifahbab2@iut-dhaka.edu>
2022-04-28 16:38:58 +0600

    first commit

---
+++ lib/catagories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import 'category_item.dart';
+
+class CatagoriesScren extends StatelessWidget {
+  // const CatagoriesScren({Key? key}) : super(key: key);
+
+  // final String categoryID;
+  // final String categoryTitle;
+
+  // CatagoriesScren(this.categoryID, this.categoryTitle);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('DeliMeals')),
+        body: GridView(
+          padding: const EdgeInsets.all(25),
+          children: DUMMY_CATEGORIES
+              .map((catItem) =>
+                  CategoryItem(catItem.id, catItem.title, catItem.color))
+              .toList(),
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20),
+        ));
+  }
+}
